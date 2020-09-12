b2753d203e008998d48c9d404579510b23cd59b4
sebaschen <sphilip1206@gmail.com>
2020-09-12 14:47:05 +0800

    run this file to checl your webcam status

---
+++ test_cam.py
@@ -0,0 +1,29 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+while(cap.isOpened()):  # check !
+    # capture frame-by-frame
+    ret, frame = cap.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+# When everything is done release the capture
+cap.release()
+cv2.destroyAllWindows()
+
+#     print('pass')
+#     if ret: # check ! (some webcam's need a "warmup")
+#         # our operation on frame come here
+#         print('frame started')
+#         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+#         # Display the resulting frame
+#         cv2.imshow('frame', gray)
+
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
+# # When everything is done release the capture
+# cap.release()
+# cv2.destroyAllWindows()
